64d3bdaa4f26c338ad770f151201fa350c279c52
tomotargz <nakamuratomota@gmail.com>
2020-06-05 15:04:05 +0000

    Make mcc be able to read code from file

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "codegen.h"
 #include "error.h"
+
+char* openFile(char* path)
+{
+    FILE* fp = fopen(path, "r");
+    if (!fp) {
+        error("failed to open file");
+        exit(1);
+    }
+    fseek(fp, 0, SEEK_END);
+    size_t size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    char* file = calloc(size + 2, sizeof(char));
+    fread(file, size, 1, fp);
+    file[size] = '\n';
+    file[size + 1] = '\0';
+    fclose(fp);
+    return file;
+}
 
 int main(int argc, char** argv)
 {
@@ -8,7 +29,8 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    char* source = argv[1];
+    char* path = argv[1];
+    char* source = openFile(path);
     Token* tokens = tokenize(source);
     Program* program = parse(tokens);
     generateCode(program);

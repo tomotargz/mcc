42421f18f77be43367f4c7436e89b037d0ed3b9e
tomotargz <nakamuratomota@gmail.com>
2020-06-05 15:22:57 +0000

    hoge

--- test/test.c
+++ test/test.c
@@ -1,3 +1,13 @@
-int main(){
+int assertEq(int expected, int actual)
+{
+    if (expected != actual) {
+        printf("test failed!! expected:%d actual:%d", expected, actual);
+    }
+    return 0;
+}
+
+int main()
+{
+    assertEq(1,({int aaaa;aaaa = 2;aaaa;}));
     return 3;
 }

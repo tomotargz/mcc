d458ca8f22e318a3b150f7ab08647c796050eb04
tomotargz <nakamuratomota@gmail.com>
2020-06-28 14:12:30 +0000

    Add rule that build 2nd generation mcc

--- Makefile
+++ Makefile
@@ -1,10 +1,14 @@
 CFLAGS=-std=c11 -g3 -O0 -static
 SRCS=$(wildcard *.c)
+HDRS=$(wildcard *.h)
 OBJS := $(SRCS:%.c=%.o)
 DEPS := $(SRCS:%.c=%.d)
 
 mcc: $(OBJS)
 	$(CC) -o mcc $(OBJS) $(LDFLAGS)
+
+mcc2: mcc $(SRCS) $(HDRS)
+	./self.sh
 
 -include $(DEPS)
 
@@ -16,7 +20,12 @@ test: mcc
 	gcc -static -O0 -o tmp tmp.s
 	./tmp
 
+test2: mcc2
+	./mcc2 "test/test.c" > tmp2.s
+	gcc -static -O0 -o tmp2 tmp2.s
+	./tmp2
+
 clean:
-	rm -f mcc *.o *~ tmp*
+	rm -f mcc mcc2 *.o *~ tmp*
 
 .PHONY: test clean
---
+++ self.sh
@@ -0,0 +1,67 @@
+#!/bin/bash -eu
+
+TMP=tmp-self
+mkdir -p $TMP
+
+compileWithMcc2(){
+    file=$1
+    cat <<EOF > $TMP/$1
+typedef struct FILE FILE;
+extern FILE *stdout;
+extern FILE *stderr;
+void *malloc(long size);
+void *calloc(long nmemb, long size);
+int *__errno_location();
+char *strerror(int errnum);
+FILE *fopen(char *pathname, char *mode);
+long fread(void *ptr, long size, long nmemb, FILE *stream);
+int feof(FILE *stream);
+static void assert() {}
+int strcmp(char *s1, char *s2);
+int printf(char *fmt, ...);
+int sprintf(char *buf, char *fmt, ...);
+long strlen(char *p);
+int strncmp(char *p, char *q);
+void *memcpy(char *dst, char *src, long n);
+char *strndup(char *p, long n);
+int isspace(int c);
+char *strstr(char *haystack, char *needle);
+long strtol(char *nptr, char **endptr, int base);
+typedef struct {
+  int gp_offset;
+  int fp_offset;
+  void *overflow_arg_area;
+  void *reg_save_area;
+} __va_elem;
+typedef __va_elem va_list[1];
+static void va_start(__va_elem *ap) {
+  __builtin_va_start(ap);
+}
+static void va_end(__va_elem *ap) {}
+EOF
+
+    grep -v '^#' mcc.h >> $TMP/$1
+    grep -v '^#' $1 >> $TMP/$1
+    sed -i 's/\bbool\b/_Bool/g' $TMP/$1
+    sed -i 's/\berrno\b/*__errno_location()/g' $TMP/$1
+    sed -i 's/\btrue\b/1/g; s/\bfalse\b/0/g;' $TMP/$1
+    sed -i 's/\bNULL\b/0/g' $TMP/$1
+    sed -i 's/INT_MAX/2147483647/g' $TMP/$1
+
+    ./mcc $TMP/$1 > $TMP/${1%.c}.s
+    gcc -c -o $TMP/${1%.c}.o $TMP/${1%.c}.s
+}
+
+for i in ./*.c; do
+    gcc -I. -c -o $TMP/${i%.c}.o $i
+done
+
+compileWithMcc2 codegen.c
+compileWithMcc2 error.c
+compileWithMcc2 main.c
+compileWithMcc2 node.c
+compileWithMcc2 parse.c
+compileWithMcc2 tokenize.c
+compileWithMcc2 type.c
+
+gcc -static -o mcc2 $TMP/*.o

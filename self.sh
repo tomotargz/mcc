c6f20a0bd3f76a7e3c5aecf1bab3a55b33eb1a9c
tomotargz <nakamuratomota@gmail.com>
2020-07-15 14:17:30 +0000

    Remove source codes except main.c from 2nd generation build

--- self.sh
+++ self.sh
@@ -60,13 +60,13 @@ for i in ./*.c; do
     gcc -I. -c -o $TMP/${i%.c}.o $i
 done
 
-compileWithMcc2 codegen.c
-compileWithMcc2 error.c
+# compileWithMcc2 codegen.c
+# compileWithMcc2 error.c
 compileWithMcc2 main.c
-compileWithMcc2 node.c
-compileWithMcc2 parse.c
-compileWithMcc2 tokenize.c
-compileWithMcc2 type.c
+# compileWithMcc2 node.c
+# compileWithMcc2 parse.c
+# compileWithMcc2 tokenize.c
+# compileWithMcc2 type.c
 
 gcc -static -o mcc2 $TMP/*.o
 echo mcc2 generated
